fd1beefb535543547592bce676809a1905db22ce
Ryan Pointer <ryankeithpointer@gmail.com>
2024-07-01 10:17:15 -0500

    Quadratic function

--- calculator-python/calculator.py
+++ calculator-python/calculator.py
@@ -1,5 +1,6 @@
+import math
 # Simple Calculator in Python
-# By: Ryan Pointer
+# By: Ryan Pointer #
 
 ###### OPERATIONS ##########################################################
 
@@ -15,11 +16,16 @@ def multiplication(x, y):
 def division(x, y):
     return x / y
 
+def quadratic(a, b, c):
+    lhs = (-b)/(2*a)
+    rhs = math.sqrt(abs(b * b - 4 * a *c))/(2*a)
+
+
 ############################################################################
 
 while True:
     choice = input("Enter Operation: \n 1.Add \n 2.Subtract \n 3.Multiply \n 4.Divide \nChoice:")
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4',):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -38,6 +44,17 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", division(num1, num2))
+
+    elif choice in ('5'):
+        try:
+            a = float(input("Enter first number: "))
+            b = float(input("Enter second number: "))
+            c = float(input("Enter third number: "))
+        except Error:
+            print("Input Error")
+            continue
+
+        print("Coefficients: ", a , "x^2 + ", b, "X +", c)
 
         repeat = input("Do you want to make another calculation? (y,n)")
         if repeat == "n":
